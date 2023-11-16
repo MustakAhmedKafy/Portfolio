570d1cbd6ee8f23be475abac0c17d93ab7b20ac2
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2023-11-16 17:51:48 +0600

    light gallery add

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <script setup>
-import Navbar from './components/Navbar.vue'
-import Banner from './components/Banner.vue'
-import About from './components/About.vue'
-import MySkill from './components/MySkill.vue'
-import Services from './components/Services.vue'
-import Experience from './components/Experience.vue'
-import project from './components/Project.vue'
+import Navbar from "./components/Navbar.vue";
+import Banner from "./components/Banner.vue";
+import About from "./components/About.vue";
+import MySkill from "./components/MySkill.vue";
+import Services from "./components/Services.vue";
+import Experience from "./components/Experience.vue";
+import Project from "./components/Project.vue";
+import Gallery from "./components/Gallery.vue";
 </script>
 
 <template>
-<Navbar/>
-<Banner />
-<About />
-<MySkill />
-<Services /> 
-<Experience />
-<project />
-<!-- <HelloWorld /> -->
+  <Navbar />
+  <Banner />
+  <About />
+  <MySkill />
+  <Services />
+  <Experience />
+  <Project />
+  <Gallery />
+  <!-- <HelloWorld /> -->
 </template>
-
---
+++ src/components/Gallery.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div v-for="(image, index) in images" :key="index" class="col-md-3 mb-4">
+        <lightgallery
+          :settings="{ speed: 500, plugins: plugins }"
+          @onInit="onInit"
+          @onBeforeSlide="onBeforeSlide"
+          elementClassNames="row"
+        >
+          <a :href="image.path">
+            <img
+              :alt="'img' + (index + 1)"
+              :src="image.path"
+              class="img-fluid"
+            />
+          </a>
+        </lightgallery>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import Lightgallery from "lightgallery/vue";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-thumbnail.css";
+import "lightgallery/css/lg-zoom.css";
+
+const images = ref([
+  { path: "/skill_icon/filmora.png" },
+  { path: "/skill_icon/filmora.png" },
+  // Add more image objects as needed
+]);
+
+const plugins = ref([lgThumbnail, lgZoom]);
+
+const onInit = () => {
+  console.log("lightGallery has been initialized");
+};
+
+const onBeforeSlide = () => {
+  console.log("calling before slide");
+};
+
+// Additional logic can be added here
+
+onMounted(() => {
+  // Additional logic on component mount
+});
+</script>
+
+<style lang="css" scoped>
+@import "lightgallery/css/lightgallery.css";
+@import "lightgallery/css/lg-thumbnail.css";
+@import "lightgallery/css/lg-zoom.css";
+</style>
