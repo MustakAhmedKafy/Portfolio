51d6dd2fc13031f2740559fc2ee29639961cef8b
Mustak Ahmed Kafy <43313297+MustakAhmedKafy@users.noreply.github.com>
2024-02-06 23:23:19 +0600

    font add

--- src/App.vue
+++ src/App.vue
@@ -23,60 +23,69 @@ import Contact from "./components/Contact.vue";
   <VideoGallery />
   <Contact />
 </template>
-  <style>
-    *{
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-    }
-    html {
-      scroll-behavior: smooth;
-    }
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html {
+  scroll-behavior: smooth;
+}
 
-    @media (max-width: 767px) {
-      html {
-        scroll-behavior: auto;
-      }
-    }
+body {
+  font-family: "Quicksand", sans-serif;
+}
+p{
+  font-weight: 500;
+  font-size: 16px;
+}
 
-    .header-section {
-      margin-bottom: 34px;
-    }
+@media (max-width: 767px) {
+  html {
+    scroll-behavior: auto;
+  }
+}
 
-    .header-section .title {
-      position: relative;
-      padding-bottom: 14px;
-      margin-bottom: 25px;
-      font-weight: 700;
-      font-size: 32px;
-    }
+.header-section {
+  margin-bottom: 34px;
+}
 
-    .header-section .title:before {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      left: 50%;
-      transform: translateX(-50%);
-      width: 50px;
-      height: 3px;
-      background-color: #0F6D73;
-      border-radius: 3px;
-    }
+.header-section .title {
+  position: relative;
+  padding-bottom: 14px;
+  margin-bottom: 25px;
+  font-weight: 700;
+  font-size: 32px;
+}
 
-    .header-section .title:after {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      left: 50%;
-      transform: translateX(30px);
-      width: 10px;
-      height: 3px;
-      background-color: rgb(54, 218, 204);
-      border-radius: 3px;
-    }
+.header-section .title:before {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 50px;
+  height: 3px;
+  background-color: #0f6d73;
+  border-radius: 3px;
+}
 
-    .header-section .description {
-      font-size: 14px;
-      color: #282828;
-    }
-  </style>
+.header-section .title:after {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(30px);
+  width: 10px;
+  height: 3px;
+  background-color: rgb(54, 218, 204);
+  border-radius: 3px;
+}
+
+.header-section .description {
+  font-size: 16px;
+  color: #282828;
+}
+</style>
